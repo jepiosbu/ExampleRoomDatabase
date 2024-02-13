39b7972688de37a2e203b35d3a2d3d14c10994a6
jepiosbu <jepiosbu@gmail.com>
2024-02-13 05:02:15 -0500

    Room

--- app/src/main/java/com/example/PlantillaRoomDatabase/ControlActivity.kt
+++ app/src/main/java/com/example/PlantillaRoomDatabase/ControlActivity.kt
@@ -1,14 +1,15 @@
 package com.example.PlantillaRoomDatabase
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.PlantillaRoomDatabase.Adapters.ControlAdapter
 import com.example.PlantillaRoomDatabase.MainActivity.Companion.rom
 import com.example.PlantillaRoomDatabase.databinding.ActivityControlBinding
 
 class ControlActivity : AppCompatActivity() {
     lateinit var binding: ActivityControlBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityControlBinding.inflate(layoutInflater)
@@ -16,15 +17,16 @@ class ControlActivity : AppCompatActivity() {
         llenar()
         refre()
     }
-    fun llenar(){
+
+    fun llenar()  {
         var obj = rom.daoControlVacuna().get()
         binding.recy.adapter = ControlAdapter(obj)
     }
-    fun refre(){
+
+    fun refre()  {
         binding.refe.setOnRefreshListener {
             binding.refe.isRefreshing = false
-            startActivity(Intent(this@ControlActivity,ControlActivity::class.java))
+            startActivity(Intent(this@ControlActivity, ControlActivity::class.java))
         }
-
     }
 }
